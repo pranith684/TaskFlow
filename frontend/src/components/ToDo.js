3c585020417b9a817f4b74c21bd70a44a53f8fca
pranith684 <solasapranith@gmail.com>
2025-07-26 11:43:24 +0530

    Update ToDo.js

--- frontend/src/components/ToDo.js
+++ frontend/src/components/ToDo.js
@@ -32,7 +32,6 @@ function Todo() {
     const [loading, setLoading] = useState(true);
 
     const api = axios.create({
-        baseURL: 'http://localhost:3001',
         headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
     });
 
